f290d8639a94a65fc3b67af7343c75de894cf239
Carlos Saito <carlos@saito.style>
2021-12-06 12:12:11 +0100

    Add pseudo-code in app.js

--- src/app.js
+++ src/app.js
@@ -23,12 +23,48 @@ function createCsvSerializer(name) {
   return serializer;
 }
 
-
 // TODO: add integration test for this function.
 // For instance: test that no antagna is added to far future rounds
+
+// ADD NEW COURSE ROOMS
+// 1. Determine current semester i.e. 20212 yyyyn n: [1 - spring | 2 - autumn]
+// 2. Fetch course rounds from Kopps that start this semester or later
+//    NOTE: We need to have some margins, perhaps current semester +/-2 semesters
+// 3. Filter by startDate so we only get course rounds where: startDate - preCreationDuration <= today
+//    - preCreationDuration -- how far in advance should a course room be created (default: 180 days)
+// 4. For each course round create source id and add to CSV
+//    - coursesData -- creates course rooms
+//    - sectionsData -- creates a section in each course room
+
+// 1. Find course rounds that should include teachers and both antagna and registered students.
+// 2. For each course round, take a list of: teachers, antagna and registered students.
+// 3. For each student:
+//    - if is registered:
+//      - Write a line to add registered
+//      - Write a line to remove antagen (optional: check in Canvas if its actually antagen)
+//    - if is antagna:
+//      - Write a line to add antagen
+// 4. For each teacher
+//    - Write a line to add them with its correct role (teacher, assistant, examiner, etc).
+
+// 1. Find course rounds that should inlcude teachers but only registered students. Antagna should be removed
+// 2. For each course round, take a list of: teachers and registered students.
+// 3. For each person (teacher, student):
+//    - Write a line to add them with its correct role
+// 4. For each course round, fetch antagna students in Canvas
+// 5. For each antagna student in Canvas write a line to remove them.
+
+// SUBMIT TO CANVAS
+// 1. Potentially merge data structures from ADD NEW COURSE ROOMS and REMOVE ADMITTED-NOT-REGISTERED STUDENTS
+// 2. Convert data structure to something we can send to Canvas
+//    NOTE: We currently use csv-files and Zip them to send as a single file
+// 3. Send files to Canvas
+// 4. Report result to logging
 async function start() {
   log.info("Run batch...");
-  const allRounds = (await getAllCourseRounds()).filter((round) => !isFarFuture(round))
+  const allRounds = (await getAllCourseRounds()).filter(
+    (round) => !isFarFuture(round)
+  );
 
   const baseDir = fs.mkdtempSync(path.join(os.tmpdir(), "sync-"));
   const dir = path.join(baseDir, "csv");
