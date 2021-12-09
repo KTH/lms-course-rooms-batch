9b76efa35ae0e7523e05fe4450721d7ba1434bc8
Emil Stenberg <esandin@gmail.com>
2021-12-09 13:32:27 +0100

    removed meta code, no longer relevant

--- src/app.js
+++ src/app.js
@@ -31,44 +31,6 @@ function createCsvSerializer(name) {
 // TODO: add integration test for this function.
 // For instance: test that no antagna is added to far future rounds
 
-// ADD NEW COURSE ROOMS
-// 1. Determine current semester i.e. 20212 yyyyn n: [1 - spring | 2 - autumn]
-// 2. Fetch course rounds from Kopps that start this semester or later
-//    NOTE: We need to have some margins, perhaps current semester +/-2 semesters
-// 3. Filter by startDate so we only get course rounds where: startDate - preCreationDuration <= today
-//    - preCreationDuration -- how far in advance should a course room be created (default: 180 days)
-// 4. For each course round create source id and add to CSV
-//    - coursesData -- creates course rooms
-//    - sectionsData -- creates a section in each course room
-
-// Enrollments for rounds that should include antagna
-// 1. Find course rounds that should include teachers and both antagna and registered students.
-// 2. For each course round, take a list of: teachers, antagna (source of thruth: UG) and registered students.
-// make sure that no student is in both antagna list and registered list
-//
-// 3. For each registeredStudent:
-//      - Write a line to add registered
-//      - Write a line to remove antagen (optional: check in Canvas if its actually antagen)
-// 4. For each antagen:
-//      - Write a line to add antagen
-//
-//  5. For each teacher:
-//    - Write a line to add them with its correct role (teacher, assistant, examiner, etc).
-
-// Enrollments for rounds that should NOT include antagna
-// 1. Find course rounds that should inlcude teachers but only registered students. Antagna should be removed
-// 2. For each course round, take a list of: teachers and registered students.
-// 3. For each person (teacher, student):
-//    - Write a line to add them with its correct role
-// 4. For each course round, fetch antagna students in Canvas
-// 5. For each antagna student in Canvas write a line to remove them.
-
-// SUBMIT TO CANVAS
-// 1. Potentially merge data structures from ADD NEW COURSE ROOMS and REMOVE ADMITTED-NOT-REGISTERED STUDENTS
-// 2. Convert data structure to something we can send to Canvas
-//    NOTE: We currently use csv-files and Zip them to send as a single file
-// 3. Send files to Canvas
-// 4. Report result to logging
 async function start() {
   /* eslint-disable */
   // START TODO remove this, it's only for test!
