3ea76afbe9cfa1491821acca4bba5d2b536f3c57
jhsware <sebastian@urbantalk.se>
2022-01-31 11:17:44 +0100

    Solve issue where CI-tests fail because we don't set process.env.CANVAS_API_URL. It isn't required for tests and a change in the npm package now throws an error when env-vars are missing.

--- src/lib/canvas.ts
+++ src/lib/canvas.ts
@@ -4,20 +4,36 @@
 import log from "skog";
 import CanvasApi from "@kth/canvas-api";
 
-const canvasApi = new CanvasApi(
-  process.env.CANVAS_API_URL,
-  process.env.CANVAS_API_TOKEN
-);
+const canvasApi =
+  process.env.CANVAS_API_URL !== undefined
+    ? new CanvasApi(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
+    : null;
+
+function _checkCanvasApiAvailable() {
+  const errMsg = [];
+  if (canvasApi === null) {
+    errMsg.push("Missing Canvas API client.");
+
+    if (!process.env.CANVAS_API_URL) {
+      errMsg.push("Env var CANVAS_API_URL has not been set.");
+    }
+  }
+  if (errMsg.length > 0) {
+    throw new Error(errMsg.join(" "));
+  }
+}
 
 interface SisImportBody {
   id: string;
 }
 export async function uploadCsvZip(fileName) {
+  _checkCanvasApiAvailable();
   return canvasApi.sendSis<SisImportBody>("accounts/1/sis_imports", fileName);
 }
 
 /** Return enrolled people as "Admitted not registered student" in a given section SIS ID */
 export async function getAntagna(sectionSisId) {
+  _checkCanvasApiAvailable();
   try {
     const enrollments = (await canvasApi
       .listItems(`sections/sis_section_id:${sectionSisId}/enrollments`, {
